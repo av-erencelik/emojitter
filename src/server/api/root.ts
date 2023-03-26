d638c1860762abb5dea8e89160a5d0d7ca8d5948
av-erencelik <erenmemo05@gmail.com>
2023-03-27 00:07:24 +0300

    pp page with ssg helper

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,7 +1,9 @@
 import { postsRouter } from "./routers/posts";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { profileRouter } from "./routers/profile";
 export const appRouter = createTRPCRouter({
   posts: postsRouter,
+  profile: profileRouter,
 });
 
 // export type definition of API
